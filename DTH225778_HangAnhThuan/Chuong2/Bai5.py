f7e2cb1b864f34ed4adcf5dfd68c3aa064cf83ea
24-HAT-02 <thuan_dth225778@student.agu.edu.vn>
2024-09-17 09:30:54 +0700

    Delete DTH225778_HangAnhThuan/Chuong2 directory

--- DTH225778_HangAnhThuan/Chuong2/Bai5.py
+++
@@ -1,4 +0,0 @@
-t="Dung dau # hoac '''"
-print("Co 2 loai ghi chu:",t)
-#Day la cach thu nhat!!!
-'''Day la cach thu 2'''
--- DTH225778_HangAnhThuan/Chuong2/Bai7.py
+++
@@ -1,3 +0,0 @@
-''' Ta se su dung ham input de nhap du lieu vao 
-Vi du : input('Nguyen Van A' )'''
-input('Nguyen Van A')
